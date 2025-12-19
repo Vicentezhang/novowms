2561f4a5c589141f54e609ede455f34ce0d097b5
Vicentezhang <127987649+Vicentezhang@users.noreply.github.com>
2025-12-19 23:54:04 +0100

    Add files via upload

---
+++ check_lucide.cjs
@@ -0,0 +1,4 @@
+const Lucide = require('lucide-react');
+console.log('FileSpreadsheet: ' + (Lucide.FileSpreadsheet ? 'Yes' : 'No'));
+console.log('CheckCircle: ' + (Lucide.CheckCircle ? 'Yes' : 'No'));
+console.log('Truck: ' + (Lucide.Truck ? 'Yes' : 'No'));
